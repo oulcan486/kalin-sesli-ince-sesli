f8ee15da53985480b0c7b5510b8e466511f58b13
oulcan486 <61511437+oulcan486@users.noreply.github.com>
2022-11-17 22:41:37 +0300

    firstcommit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char harf='E';
+		 if(harf=='A' || harf=='I' || harf=='O' || harf=='U') {
+			 System.out.println("Kalin harf");
+		 }else if(harf=='E' || harf=='İ' || harf=='Ö' || harf=='Ü') {
+			 System.out.println("Ince harf");
+		 }else {
+			 System.out.println("Hicbiri değil");
+		 }
+	}
+
+}
